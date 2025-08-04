6e96eb18f31df8aacf236f493202721c3c45d3a2
Yash <nirmalyash721@gmail.com>
2025-08-04 14:19:32 +0530

    Initial commit - add folder structure and basic setup

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/auth/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Charusat Video Conference',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
